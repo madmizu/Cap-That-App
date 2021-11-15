614cf2625178906022adab36b2b0c3711f599a2b
madmizu <91926862+madmizu@users.noreply.github.com>
2021-11-15 12:42:02 -1000

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {})
+
+
+// const likeBtn = document.getElementById("like-button")
+// const likeNum = document.getElementById("like-count")
+// const image = document.getElementsByClassName("image-card")
+
+// likeBtn.addEventListener("click", (e) => console.log(e.target.value))
+
+// function init() {
+//   image.forEach( )
+//   e.forEach(++likeNum)
+// }
+
+
+https://api.imgur.com/3/gallery/search?q=funny
