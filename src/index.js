45daeb22c3cc1ca8be3d04256a14f71c91fcccee
Madison Shimizu <mhshimizu@outlook.com>
2021-11-18 10:45:07 -1000

    new form created

--- src/index.js
+++ src/index.js
@@ -7,44 +7,67 @@ fetch(URL)
   .then((resp) => resp.json())
   .then(loadImages);
 
+
+  const newForm = document.getElementById("newGifEntry")
+  newForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    
+    const imageGif = document.createElement("img");
+    imageGif.setAttribute("id", "card-image");
+    imageGif.setAttribute("class", "image");
+    imageGif.src = e.target.newImageId.value
+    imageGif.alt = `image URL ${e.target.newImageId.value}`
+    renderImage(imageGif)
+    e.target.reset()
+  })
+  
 function loadImages(gifImage) {
   const entries = Object.entries(gifImage);
   const array = entries[0][1];
-  array.forEach(renderImage);
+  array.forEach(renderGif);
 }
 
-function renderImage(gifImage) {
+function renderGif (gifImage) {
+  const imageGif = document.createElement("img");
+  imageGif.setAttribute("id", "card-image");
+  imageGif.setAttribute("class", "image");
+  imageGif.src = gifImage.media[0].gif.url;
+  imageGif.alt = gifImage.content_description;
+  renderImage(imageGif)
+}
+
+function renderImage(imageGif) {
   const captionButton = document.createElement("button");
   const captionForm = document.createElement("form");
   const imageCard = document.createElement("div");
-  const imageGif = document.createElement("img");
   const bodyOfPage = document.getElementById("body");
   const formInput = document.createElement("input");
-  const captionList = document.createElement("ol");
+  const captionList = document.createElement("ul");
+  const imageDiv = document.createElement("div")
+  const topCaption = document.createElement("h2")
 
-  captionButton.innerText = "post";
+  captionButton.innerText = "Post";
   captionButton.setAttribute("type", "submit");
   captionButton.setAttribute("class", "caption-button");
 
-  formInput.setAttribute("placeHolder", "cap me...");
+  formInput.setAttribute("placeHolder", "Cap me...");
   formInput.setAttribute("type", "text");
   formInput.setAttribute("name", "caption");
   formInput.setAttribute("id", "captionInput");
 
   captionForm.setAttribute("class", "caption-form");
   imageCard.setAttribute("class", "image-card");
-
-  imageGif.setAttribute("id", "card-image");
-  imageGif.setAttribute("class", "image");
-  imageGif.src = gifImage.media[0].gif.url;
-  imageGif.alt = gifImage.content_description;
+  imageDiv.setAttribute("class", "gifAndCaption")
 
   bodyOfPage.append(imageCard);
-  imageCard.append(imageGif);
+  imageDiv.append(imageGif);
+  imageCard.append(imageDiv)
   imageCard.append(captionForm);
   captionForm.appendChild(formInput);
   captionForm.appendChild(captionButton);
   imageCard.append(captionList);
+  imageDiv.append(topCaption);
+
 
   captionForm.addEventListener("submit", (e) => {
 	e.preventDefault()
@@ -54,6 +77,7 @@ function renderImage(gifImage) {
     const newCaptionEntry = e.target.captionInput.value
     eachCaption.textContent = newCaptionEntry;
     e.target.reset()
+    console.log(newCaptionEntry)
 
     const likeBtn = document.createElement("button")
     likeBtn.setAttribute("class", "like-button")
@@ -69,10 +93,19 @@ function renderImage(gifImage) {
     likeBtn.addEventListener("click", (e) => {
       ++i 
       numOfLikes.textContent = i
-      console.log(i)
+      renderTopCaption (newCaptionEntry, numOfLikes)
         
       })
   
-    })
-}
+    function renderTopCaption (newCaptionEntry, numOfLikes) {
+      
+    }
 
+    })
+
+    
+
+
+
+
+}
